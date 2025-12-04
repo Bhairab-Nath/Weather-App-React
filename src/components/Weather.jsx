e4337d722ebd129670de19e896eee185f7a518de
Bhairab-Nath <nathbhairab22@gmail.com>
2025-12-04 17:21:51 +0545

    Updated icon

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,5 @@
 import search_icon from '../assets/search.png'
-import clear_icon from '../assets/clear.png'
-import cloud_icon from '../assets/cloud.png'
-import drizzle_icon from '../assets/drizzle.png'
 import humidity_icon from '../assets/humidity.png'
-import rain_icon from '../assets/rain.png'
-import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 import { useEffect, useRef, useState } from 'react'
 
@@ -13,24 +8,6 @@ const Weather = () => {
 
     const inputRef = useRef()
     const [weatherData, setWeatherData] = useState(false)
-
-    const icons = {
-        "01d": clear_icon,
-        "01n": clear_icon,
-        "02d": cloud_icon,
-        "02n": cloud_icon,
-        "03d": cloud_icon,
-        "03n": cloud_icon,
-        "04d": drizzle_icon,
-        "04n": drizzle_icon,
-        "09d": rain_icon,
-        "09n": rain_icon,
-        "10d": rain_icon,
-        "10n": rain_icon,
-        "13d": snow_icon,
-        "13n": snow_icon
-
-    }
 
     const search = async (city) => {
         if (city === "") {
@@ -48,7 +25,9 @@ const Weather = () => {
                 return
             }
 
-            const icon = icons[data.weather[0].icon] || clear_icon
+          
+            const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            
             setWeatherData({
                 humidity: data.main.humidity,
                 windSpeed: data.wind.speed,
